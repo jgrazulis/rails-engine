628550c9326adb9f86846aa853682514fea1051e
Jessica Grazulis <jlgrazulis@gmail.com>
2022-04-01 08:02:12 -0600

    Add appropriate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchant_search#find'
+      get '/items/find_all', to: 'item_search#find_all'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
-      resources :items, except: [:edit]
+      resources :items, except: [:edit] do
+        resources :merchant, only: [:index], controller: :item_merchant
+      end
     end
   end
 end
